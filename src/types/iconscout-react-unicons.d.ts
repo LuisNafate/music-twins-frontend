5ad3ef38363695ce6ea3d78f8c65faaf0701976a
LuisNafate <luisnafate@gmail.com>
2026-04-20 14:53:39 -0600

    types(iconos): declarar modulo iconscout unicons

---
+++ src/types/iconscout-react-unicons.d.ts
@@ -0,0 +1,34 @@
+declare module '@iconscout/react-unicons' {
+  import type { FC, SVGProps } from 'react'
+
+  export type UniconProps = SVGProps<SVGSVGElement> & {
+    size?: string | number
+    color?: string
+  }
+
+  export const UilArrowRight: FC<UniconProps>
+  export const UilBell: FC<UniconProps>
+  export const UilBolt: FC<UniconProps>
+  export const UilChartGrowth: FC<UniconProps>
+  export const UilChartLine: FC<UniconProps>
+  export const UilCommentAltLines: FC<UniconProps>
+  export const UilEstate: FC<UniconProps>
+  export const UilFavorite: FC<UniconProps>
+  export const UilFire: FC<UniconProps>
+  export const UilHeadphones: FC<UniconProps>
+  export const UilHeadphonesAlt: FC<UniconProps>
+  export const UilHeartRate: FC<UniconProps>
+  export const UilLocationArrow: FC<UniconProps>
+  export const UilMessage: FC<UniconProps>
+  export const UilMusic: FC<UniconProps>
+  export const UilPlay: FC<UniconProps>
+  export const UilSearch: FC<UniconProps>
+  export const UilShareAlt: FC<UniconProps>
+  export const UilShieldCheck: FC<UniconProps>
+  export const UilSignOutAlt: FC<UniconProps>
+  export const UilSpinnerAlt: FC<UniconProps>
+  export const UilStar: FC<UniconProps>
+  export const UilUser: FC<UniconProps>
+  export const UilUserCircle: FC<UniconProps>
+  export const UilUsersAlt: FC<UniconProps>
+}
